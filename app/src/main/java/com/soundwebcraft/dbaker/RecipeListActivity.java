50d55a7fdd19c53566903203f364c6fc3e407e35
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-07 09:16:22 +0100

    host recipe list fragment

--- app/src/main/java/com/soundwebcraft/dbaker/RecipeListActivity.java
+++ app/src/main/java/com/soundwebcraft/dbaker/RecipeListActivity.java
@@ -1,7 +1,11 @@
 package com.soundwebcraft.dbaker;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.soundwebcraft.dbaker.fragments.RecipeListFragment;
 
 public class RecipeListActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class RecipeListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+
+        // host RecipeListFragment
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new RecipeListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
