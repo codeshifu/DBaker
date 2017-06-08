6d92d9e965a024747dd3c45eff365b4b449fdf0d
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-08 10:42:13 +0100

    activity uses toolbar

--- app/src/main/java/com/soundwebcraft/dbaker/RecipeListActivity.java
+++ app/src/main/java/com/soundwebcraft/dbaker/RecipeListActivity.java
@@ -1,9 +1,10 @@
 package com.soundwebcraft.dbaker;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.soundwebcraft.dbaker.fragments.RecipeListFragment;
 
@@ -14,6 +15,8 @@ public class RecipeListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
+        Toolbar appBar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(appBar);
 
         // host RecipeListFragment
         FragmentManager fm = getSupportFragmentManager();
