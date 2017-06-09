85a4d0faa2f85f2afba5bbb6bbcf59674cde2d39
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-09 18:20:43 +0100

    recipe detail fragment

---
+++ app/src/main/java/com/soundwebcraft/dbaker/RecipeDetailFragment.java
@@ -0,0 +1,57 @@
+package com.soundwebcraft.dbaker;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.soundwebcraft.dbaker.dummy.Recipe;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class RecipeDetailFragment extends Fragment {
+    public static final String ARG_RECIPE = "recipe";
+
+    private Recipe mRecipe;
+    private Unbinder unbinder;
+
+    public RecipeDetailFragment() {
+        // Required empty public constructor
+    }
+
+    public static RecipeDetailFragment newInstance(Recipe recipe) {
+        RecipeDetailFragment fragment = new RecipeDetailFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_RECIPE, recipe);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mRecipe = (Recipe) getArguments().getSerializable(ARG_RECIPE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        unbinder = ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
