6bf417bf861f356735d6996f673cccadbdb99d35
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-10 07:09:50 +0100

    hook up recipedetailfragment with recipedetailactivity

--- app/src/main/java/com/soundwebcraft/dbaker/RecipeDetail.java
+++ app/src/main/java/com/soundwebcraft/dbaker/RecipeDetail.java
@@ -1,13 +1,30 @@
 package com.soundwebcraft.dbaker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.soundwebcraft.dbaker.dummy.Recipe;
 
 public class RecipeDetail extends AppCompatActivity {
+
+    public static final String EXTRA_RECIPE = "recipe";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
+        // host RecipeListFragment
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        Recipe recipe = (Recipe) getIntent().getSerializableExtra(EXTRA_RECIPE);
+        if (fragment == null) {
+            fragment = RecipeDetailFragment.newInstance(recipe);
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
