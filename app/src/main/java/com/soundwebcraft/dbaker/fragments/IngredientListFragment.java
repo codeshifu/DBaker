816335c877da46d5ac3dfc9082e69748a1aaef78
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-11 15:37:05 +0100

    added fragment adapter skeleton

--- app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragment.java
+++ app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragment.java
@@ -1,8 +1,10 @@
 package com.soundwebcraft.dbaker.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,5 +36,42 @@ public class IngredientListFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
+        private Context mContext;
+
+        public IngredientAdapter(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View v = inflater.inflate(R.layout.list_item_ingredient, parent, false);
+
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.bind();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 15;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            public ViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+            }
+
+            public void bind() {
+
+            }
+        }
     }
 }
