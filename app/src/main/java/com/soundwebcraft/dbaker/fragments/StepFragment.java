46571a2525d823afefc81f506e10ad0d50dbf616
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-11 16:55:34 +0100

    setup adapter skeleton

--- app/src/main/java/com/soundwebcraft/dbaker/fragments/StepFragment.java
+++ app/src/main/java/com/soundwebcraft/dbaker/fragments/StepFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,5 +53,42 @@ public class StepFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    public class StepAdapter extends RecyclerView.Adapter<StepAdapter.ViewHolder> {
+        private Context adapterContext;
+
+        public StepAdapter(Context context) {
+            adapterContext = context;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(adapterContext);
+            View v = inflater.inflate(R.layout.list_item_step, parent, false);
+
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.bind();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 8;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            public ViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+            }
+
+            public void bind() {
+
+            }
+        }
     }
 }
