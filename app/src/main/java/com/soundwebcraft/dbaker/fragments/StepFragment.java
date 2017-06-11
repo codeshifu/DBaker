4793c015a13534ceb154cad65ab54463cdfb93dd
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-11 16:46:40 +0100

    added new fragment (StepFragment) class

---
+++ app/src/main/java/com/soundwebcraft/dbaker/fragments/StepFragment.java
@@ -0,0 +1,56 @@
+package com.soundwebcraft.dbaker.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.soundwebcraft.dbaker.R;
+import com.soundwebcraft.dbaker.utils.EmptyStateRecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+public class StepFragment extends Fragment {
+    @BindView(R.id.recyclerview)
+    EmptyStateRecyclerView mRecyclerview;
+    @BindView(R.id.empty_state_feedback)
+    TextView mEmptyStateFeedback;
+    @BindView(R.id.empty_view)
+    LinearLayout mEmptyView;
+
+    private Unbinder unbinder;
+    private Context mContext;
+
+    public StepFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_step_list, container, false);
+        unbinder = ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
