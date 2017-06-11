bab90b8d537f73b67f271830c6eaecd40edd4ab4
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-11 15:50:14 +0100

    wired up recyclerview

--- app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragment.java
+++ app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragment.java
@@ -4,30 +4,52 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.soundwebcraft.dbaker.R;
+import com.soundwebcraft.dbaker.utils.EmptyStateRecyclerView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 
 public class IngredientListFragment extends Fragment {
 
+    @BindView(R.id.recyclerview) EmptyStateRecyclerView mRecyclerview;
+    @BindView(R.id.empty_state_feedback) TextView mEmptyStateFeedback;
+    @BindView(R.id.empty_view) LinearLayout mEmptyView;
+
     private Unbinder unbinder;
+    private Context mContext;
 
     public IngredientListFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
-        ButterKnife.bind(this, v);
+        unbinder = ButterKnife.bind(this, v);
+
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(mContext));
+        mRecyclerview.addItemDecoration(new DividerItemDecoration(mContext, DividerItemDecoration.VERTICAL));
+        mRecyclerview.setHasFixedSize(true);
+        mRecyclerview.setAdapter(new IngredientAdapter(mContext));
 
         return v;
     }
@@ -39,15 +61,15 @@ public class IngredientListFragment extends Fragment {
     }
 
     public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
-        private Context mContext;
+        private Context adapterContext;
 
         public IngredientAdapter(Context context) {
-            mContext = context;
+            adapterContext = context;
         }
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(mContext);
+            LayoutInflater inflater = LayoutInflater.from(adapterContext);
             View v = inflater.inflate(R.layout.list_item_ingredient, parent, false);
 
             return new ViewHolder(v);
@@ -63,7 +85,7 @@ public class IngredientListFragment extends Fragment {
             return 15;
         }
 
-        class ViewHolder extends RecyclerView.ViewHolder{
+        class ViewHolder extends RecyclerView.ViewHolder {
             public ViewHolder(View itemView) {
                 super(itemView);
                 ButterKnife.bind(this, itemView);
