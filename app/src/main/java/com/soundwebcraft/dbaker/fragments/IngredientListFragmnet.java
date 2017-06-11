743a953cad9e2344533f699f45373efde7775a53
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-11 15:01:01 +0100

    added new fragment (IngredientList) class

---
+++ app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragmnet.java
@@ -0,0 +1,34 @@
+package com.soundwebcraft.dbaker.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.soundwebcraft.dbaker.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+public class IngredientListFragmnet extends Fragment {
+
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
+        ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
