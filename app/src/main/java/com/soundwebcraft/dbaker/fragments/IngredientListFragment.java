1776555196a6e52184dbd93a2a0bcfd9b7ed3fd3
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-15 07:26:03 +0100

    used android timeline library to display recipe ingredients

--- app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragment.java
+++ app/src/main/java/com/soundwebcraft/dbaker/fragments/IngredientListFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,8 +12,10 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.github.vipulasri.timelineview.TimelineView;
 import com.soundwebcraft.dbaker.R;
 import com.soundwebcraft.dbaker.utils.EmptyStateRecyclerView;
+import com.soundwebcraft.dbaker.utils.VectorDrawableUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,9 +24,12 @@ import butterknife.Unbinder;
 
 public class IngredientListFragment extends Fragment {
 
-    @BindView(R.id.recyclerview) EmptyStateRecyclerView mRecyclerview;
-    @BindView(R.id.empty_state_feedback) TextView mEmptyStateFeedback;
-    @BindView(R.id.empty_view) LinearLayout mEmptyView;
+    @BindView(R.id.recyclerview)
+    EmptyStateRecyclerView mRecyclerview;
+    @BindView(R.id.empty_state_feedback)
+    TextView mEmptyStateFeedback;
+    @BindView(R.id.empty_view)
+    LinearLayout mEmptyView;
 
     private Unbinder unbinder;
     private Context mContext;
@@ -46,8 +50,7 @@ public class IngredientListFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_ingredient_list, container, false);
         unbinder = ButterKnife.bind(this, v);
 
-        mRecyclerview.setLayoutManager(new LinearLayoutManager(mContext));
-        mRecyclerview.addItemDecoration(new DividerItemDecoration(mContext, DividerItemDecoration.VERTICAL));
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
         mRecyclerview.setHasFixedSize(true);
         mRecyclerview.setNestedScrollingEnabled(false);
         mRecyclerview.setAdapter(new IngredientAdapter(mContext));
@@ -73,7 +76,7 @@ public class IngredientListFragment extends Fragment {
             LayoutInflater inflater = LayoutInflater.from(adapterContext);
             View v = inflater.inflate(R.layout.list_item_ingredient, parent, false);
 
-            return new ViewHolder(v);
+            return new ViewHolder(v, viewType);
         }
 
         @Override
@@ -82,17 +85,29 @@ public class IngredientListFragment extends Fragment {
         }
 
         @Override
+        public int getItemViewType(int position) {
+            return TimelineView.getTimeLineViewType(position, getItemCount());
+        }
+
+        @Override
         public int getItemCount() {
             return 15;
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
-            public ViewHolder(View itemView) {
+            @BindView(R.id.time_marker)
+            TimelineView mTimelineView;
+
+
+            public ViewHolder(View itemView, int viewType) {
                 super(itemView);
                 ButterKnife.bind(this, itemView);
+                mTimelineView.initLine(viewType);
             }
 
             public void bind() {
+                //mTimelineView.setMarker(ContextCompat.getDrawable(mContext, R.drawable.ic_marker), ContextCompat.getColor(mContext, R.color.colorPrimary));
+                mTimelineView.setMarker(VectorDrawableUtils.getDrawable(mContext, R.drawable.ic_marker_active, R.color.colorPrimaryDark));
 
             }
         }
