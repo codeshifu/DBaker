903ed8d2c00c4e212b0ff99b24c4ffce8d5fb280
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-19 06:33:24 +0100

    added more utils methods

--- app/src/main/java/com/soundwebcraft/dbaker/db/DbUtils.java
+++ app/src/main/java/com/soundwebcraft/dbaker/db/DbUtils.java
@@ -2,12 +2,15 @@ package com.soundwebcraft.dbaker.db;
 
 import com.soundwebcraft.dbaker.data.model.Recipe;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.soundwebcraft.dbaker.db.RecipeEntity.*;
 
 public class DbUtils {
     public static void saveStepEntities(List<Recipe.Steps> steps, RecipeEntity recipeEntity) {
         for (int i = 0; i < steps.size(); i++) {
-            RecipeEntity.StepEntity stepEntity = new RecipeEntity.StepEntity(
+            StepEntity stepEntity = new StepEntity(
                     steps.get(i).id,
                     steps.get(i).shortdescription,
                     steps.get(i).description,
@@ -21,7 +24,7 @@ public class DbUtils {
 
     public static void saveIngredientEntities(List<Recipe.Ingredients> ingredients, RecipeEntity recipeEntity) {
         for (int i = 0; i < ingredients.size(); i++) {
-            RecipeEntity.IngredientEntity ingredientEntity = new RecipeEntity.IngredientEntity(
+            IngredientEntity ingredientEntity = new IngredientEntity(
                     ingredients.get(i).quantity,
                     ingredients.get(i).measure,
                     ingredients.get(i).ingredient,
@@ -29,5 +32,45 @@ public class DbUtils {
             );
             ingredientEntity.save();
         }
+    }
+
+    public static Recipe convertToRecipeObject(RecipeEntity recipeEntity) {
+        List<RecipeEntity.StepEntity> se = new ArrayList<>();
+        List<RecipeEntity.IngredientEntity> ie = new ArrayList<>();
+        String whereClause = "recipe = ?";
+        ie = IngredientEntity.find(IngredientEntity.class, whereClause, String.valueOf(recipeEntity.getId()));
+        se = StepEntity.find(StepEntity.class, whereClause, String.valueOf(recipeEntity.getId()));
+
+        List<Recipe.Ingredients> ingredients = extractIngredients(ie);
+        List<Recipe.Steps> steps = extractSteps(se);
+
+        return new Recipe(
+                recipeEntity.getRecipeId(),
+                recipeEntity.getName(),
+                ingredients, steps, recipeEntity.getServings(), recipeEntity.getImage()
+        );
+    }
+
+    public static List<Recipe.Steps> extractSteps(List<StepEntity> se) {
+        List<Recipe.Steps> steps = new ArrayList<>();
+        for (StepEntity entity : se) {
+            steps.add(new Recipe.Steps(
+                    entity.getStepId(),
+                    entity.getShortdescription(),
+                    entity.getDescription(),
+                    entity.getVideourl(),
+                    entity.getThumbnailUrl()
+            ));
+        }
+        return steps;
+    }
+
+    public static List<Recipe.Ingredients> extractIngredients(List<RecipeEntity.IngredientEntity> ie) {
+        List<Recipe.Ingredients> ingredients = new ArrayList<>();
+        for (IngredientEntity entity : ie) {
+            ingredients.add(new Recipe.Ingredients(entity.getQuantity(), entity.getMeasure(), entity.getIngredient()
+            ));
+        }
+        return ingredients;
     }
 }
