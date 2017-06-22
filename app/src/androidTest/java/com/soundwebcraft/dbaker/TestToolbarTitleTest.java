f93f58282cb15256cf872c93d30c6d729c440134
Olushi Luqman O <olushilukmon03@gmail.com>
2017-06-22 16:51:06 +0100

    espresso test toolbar title

---
+++ app/src/androidTest/java/com/soundwebcraft/dbaker/TestToolbarTitleTest.java
@@ -0,0 +1,27 @@
+package com.soundwebcraft.dbaker;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class TestToolbarTitleTest {
+    @Rule public ActivityTestRule<RecipeListActivity> mActivityTestRule =
+            new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void testToolbarTitleDisplayed () {
+        String app_name = mActivityTestRule.getActivity().getResources()
+                .getString(R.string.app_name);
+
+        onView(withId(R.id.toolbar_title)).check(matches(withText(app_name)));
+    }
+}
